d55b03bdb6edfa88e5474b5a4c8190049da60699
rayan <rayancooray123456@gmail.com>
2024-05-23 00:33:10 +0530

    Did the add employee form and the model and apis accordingly

---
+++ assets/javascript/Employee.js
@@ -0,0 +1,12 @@
+import { EmployeeAPI } from "./api/EmployeeAPI.js";
+import { ModelEmployee } from "./model/Model-Employee.js";
+
+
+
+// Other parts of your code
+const employee_name = $("#emName").val();
+const employee_gender = $("#gender").val();
+const employee_DOB = $("#date_of_birth").val();
+const employee_tel = $("#contact_no").val();
+const employee_em_tel = $("#emergency_contact_no").val();
+const employee_dp = $("#profile_picture").val();
---
+++ assets/javascript/model/Model-Employee.js
@@ -0,0 +1,26 @@
+export class ModelEmployee {
+    constructor(code,name,DOB,gender,date_of_birth,contact_no,emergency_contact_no,profile_picture,accessRole
+                ,designation,name_of_the_guardian,date_of_joining,address1,address2,address3,address4,postalCode,branch,statu,email) {
+        this.code = code;
+        this.name = name;
+        this.gender = gender;
+        this.date_of_birth = date_of_birth;
+        this.contact_no = contact_no;
+        this.emergency_contact_no = emergency_contact_no;
+        this.profile_picture = profile_picture;
+        this.accessRole = accessRole;
+        this.designation = designation;
+        this. name_of_the_guardian = name_of_the_guardian;
+        this.date_of_joining = date_of_joining;
+        this.address1 = address1;
+        this.address2 = address2;
+        this.address3 = address3;
+        this.address4 = address4;
+        this.postalCode = postalCode;
+        this.branch = branch;
+        this.status = statu;
+        this.email = email;
+
+
+    }
+}
