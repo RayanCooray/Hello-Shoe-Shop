5c219f6dab6745b33485ff0fc63ad2086e5c2d87
rayan <rayancooray123456@gmail.com>
2024-05-28 10:56:53 +0530

    Supplier form created with supplier form table and styled the css Supplier Model Created

---
+++ assets/javascript/model/Model-Supplier.js
@@ -0,0 +1,16 @@
+export class ModelSupplier{
+    constructor(supplierId, supplier_name, category, address_line_01,address_line_02,address_line_03,address_line_04,address_line_05,address_line_06,contact_no_1,contact_no_2,email) {
+        this.supplierId = supplierId;
+        this.supplier_name = supplier_name;
+        this.category = category;
+        this.address_line_01 = address_line_01;
+        this.address_line_02 = address_line_02;
+        this.address_line_03 = address_line_03;
+        this.address_line_04 = address_line_04;
+        this.address_line_05 = address_line_05;
+        this.address_line_06 = address_line_06;
+        this.contact_no_1 = contact_no_1;
+        this.contact_no_2 = contact_no_2;
+        this.email = email;
+    }
+}
